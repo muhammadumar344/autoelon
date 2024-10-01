85ebe86bdaac95ea6b9f00709cb84778001f76bd
muhammadumar344 <sirojidinov2011@gmail.com>
2024-10-01 23:54:34 +0500

    responsive

--- src/assets/companents/Navbar/navbar.jsx
+++ src/assets/companents/Navbar/navbar.jsx
@@ -1,27 +1,30 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
-import "../Navbar/navbar.css"
+import "../Navbar/navbar.css";
 
 function Navbar() {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleNavbar = () => {
+        setIsOpen(!isOpen);
+    };
+
     return (
-        <>
         <div className="navbar">
-            <div className="navbar-list">
-                <div className="navbar-item">
-                <h2 className="logo-name">Avtoelon.uz</h2>
-                </div>
-                <div className="navbar-item">
-                    <Link to={"/"} className="pages-text">Home</Link>
-                    <Link to={"/About"} className="pages-text">About</Link>
-                    <Link to={"/setting"} className="pages-text">Settings</Link>
-                    <Link to={"/Contact"} className="pages-text">Contact</Link>
-                    <Link to={"/login"} className="pages-text">Log in</Link>
-                    <Link to={"/sign"} className="pages-text">Sign up</Link>
-                </div>
+            <h2 className="logo-name">Avtoelon.uz</h2>
+            <div className={`navbar-item ${isOpen ? "active" : ""}`}>
+                <Link to={"/"} className="pages-text">Home</Link>
+                <Link to={"/About"} className="pages-text">About</Link>
+                <Link to={"/setting"} className="pages-text">Settings</Link>
+                <Link to={"/Contact"} className="pages-text">Contact</Link>
+                <Link to={"/login"} className="pages-text">Log in</Link>
+                <Link to={"/sign"} className="pages-text">Sign up</Link>
+            </div>
+            <div className="icon" onClick={toggleNavbar}>
+                ☰
             </div>
         </div>
-        
-        </>
-    )
+    );
 }
 
 export default Navbar;
